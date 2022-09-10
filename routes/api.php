16d56f5570562ed82dda567134f050992424b350
Saiful Islam <saifhd85@gmail.com>
2022-09-10 20:17:31 +0530

    limit the access to the api endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,28 +2,19 @@
 
 use App\Http\Controllers\Api\V1\ProductController;
 use App\Http\Controllers\Api\V1\SellerProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::prefix('v1')->group(function () {
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::get('/products/{product}', [ProductController::class, 'show']);
-    Route::get('/products/{product}/sellers', [ProductController::class, 'productSellers']);
+
+    Route::controller(ProductController::class)
+        ->prefix('/products')
+        ->group(function () {
+            Route::get('/', 'index');
+            Route::get('/{product}', 'show');
+            Route::get('/{product}/sellers', 'productSellers');
+        });
 
     Route::get('sellers/{id}/products', SellerProductController::class);
+    
 });
